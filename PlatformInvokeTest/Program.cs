dc76dddfc21b9c7e962a4f2173def7ececd7ae54
fo40225 <fo40225@hotmail.com>
2017-07-01 16:28:49 +0800

    https://msdn.microsoft.com/en-us/library/aa288468.aspx

--- PlatformInvokeTest/Program.cs
+++ PlatformInvokeTest/Program.cs
@@ -1,15 +1,26 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace PlatformInvokeTest
+﻿namespace PlatformInvokeTest
 {
-    class Program
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    using System.Runtime.InteropServices;
+
+    internal class Program
     {
-        static void Main(string[] args)
+        [DllImport("msvcrt.dll", CallingConvention = CallingConvention.Cdecl)]
+        public static extern int puts(string c);
+
+        [DllImport("msvcrt.dll", CallingConvention = CallingConvention.Cdecl)]
+        internal static extern int _flushall();
+
+        private static void Main(string[] args)
+
         {
+            puts("hello, P/Invoke");
+            _flushall();
         }
     }
 }
